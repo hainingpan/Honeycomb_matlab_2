260e304feda670316cf258ee6af6d3229100d668
Haining Pan <jack_pan1994@126.com>
2018-09-24 14:29:41 -0400

    20180924

--- test.m
+++ test.m
@@ -1,14 +1,28 @@
-index=1;
-d=parameters.d;
-for i=1:5
-tic
-set=[50];
-% set=[50,100,200,300,400];
-    fprintf('%d',i);
-    parameters.NN=set(i);
-	rs(i)=tV(index,0,2*d,0,d,parameters)
-rt(i)=toc
+function linindex=test(kindex,b)
+q=5;
+
+        kxindex=ceil(kindex/q);
+        kyindex=mod((kindex-1),q)+1;
+        switch b
+            case 1
+                kxbindex=kxindex;
+                kybindex=mod(kyindex,q)+1;
+            case 2
+                kxbindex=mod(kxindex,q)+1;
+                kybindex=mod(kyindex,q)+1;
+            case 3
+                kxbindex=mod(kxindex,q)+1;
+                kybindex=kyindex;
+            case 4
+                kxbindex=kxindex;
+                kybindex=mod(kyindex-2,q)+1;
+            case 5
+                kxbindex=mod(kxindex-2,q)+1;
+                kybindex=mod(kyindex-2,q)+1;
+            case 6
+                kxbindex=mod(kxindex-2,q)+1;
+                kybindex=kyindex;
+        end
+        linindex=(kxbindex-1)*q+kybindex;
+     
 end
-
-
-
