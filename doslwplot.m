ee2c0858a9be54307ce30163bf8e4d3479b9f76c
Haining Pan <jack_pan1994@126.com>
2018-10-03 12:41:50 -0400

    20181003

--- doslwplot.m
+++ doslwplot.m
@@ -1,5 +1,17 @@
-store=load('cp.mat');
-map=store.intensitymap;
-figure;
-plot(enlist,squeeze(sum(map,[1,2])))
-% surf(klist,enlist,squeeze(map(:,50,:))','edgecolor','none');view(2)
+% store=load('cp.mat');
+% map=store.intensitymap;
+% figure;
+% plot(enlist,squeeze(sum(map,[1,2])))
+% % surf(klist,enlist,squeeze(map(:,50,:))','edgecolor','none');view(2)
+d=parameters.d;
+den=100;
+eden=200;
+enlist=linspace(-0.005,0.015,eden);
+klist=linspace(-2*pi*6/(3*d),2*pi*6/(3*d),den);
+intensitymap=zeros(den,eden);    
+ for ii=1:den
+     ky=klist(ii);  
+%      kx=klist(ii);
+     disp(ky);      
+     intensitymap(ii,:)=lw(0,ky,enlist,parameters);
+end
